2c61b774915232064988ef226f373c1dd47cd70a
Yusup Firmansyah <yhusup6@gmail.com>
2021-09-05 11:47:38 +0700

    Install sbadmin2+template, partials view, controller, router

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+// Router Admin
+var adminRouter = require('./routes/admin');
 
 var app = express();
 
@@ -18,9 +20,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/sb-admin-2', express.static(path.join(__dirname, 'node_modules/startbootstrap-sb-admin-2')))
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/admin', adminRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
