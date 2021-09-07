2297eb93d5895f40eb41924c748763922b9a4f0c
Yusup Firmansyah <yhusup6@gmail.com>
2021-09-07 17:33:46 +0700

    Install method-overrid, udpate+delete Category

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -12,21 +12,25 @@ module.exports = {
     },
 
     addCategory:async (req,res) => {
-        console.log("add kepanggil");
         const {name} = req.body;
         await Category.create({name});
         res.redirect('/admin/category');
     },
     
     editCategory:async (req,res) => {
-        console.log("Edit KEPANGGIL");
+        const {id, name} = req.body;
+        const category = await Category.findOne({_id:id})
+        category.name = name;
+        await category.save();
         res.redirect('/admin/category');
     },
     
-    deleteCategory:async (req,res) => {
-        console.log("Delete KEPANGGIL");
+    deleteCategory: async (req, res) => {
+        const { id } = req.params;
+        const category = await Category.findOne({ _id: id });
+        await category.remove();
         res.redirect('/admin/category');
-    },
+      },
 
     viewBank:(req,res) => {
         res.render('admin/bank/view_bank');
