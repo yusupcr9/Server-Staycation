e003833dbde0985034bf4d8af854b4aa5a0c0333
Yusup Firmansyah <yhusup6@gmail.com>
2021-10-22 22:13:11 +0700

    API Booking page

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -3,6 +3,8 @@ const Treasure = require("../models/Activity");
 const Traveler = require("../models/Booking");
 const Category = require("../models/Category");
 const Bank = require("../models/Bank");
+const Booking = require("../models/Booking");
+const Member = require("../models/Member");
 
 module.exports = {
   landingPage: async (req, res) => {
@@ -67,6 +69,7 @@ module.exports = {
       res.status(500).json({ message: "Internal server error" });
     }
   },
+
   detailPage: async (req, res) => {
     try {
       const { id } = req.params;
@@ -95,5 +98,81 @@ module.exports = {
     } catch (error) {
       res.status(500).json({ message: "Internal server error" });
     }
+  },
+
+  bookingPage: async (req, res) => {
+    const {
+      idItem,
+      duration,
+      // price,
+      bookingStartDate,
+      bookingEndDate,
+      firstName,
+      lastName,
+      email,
+      phoneNumber,
+      accountHolder,
+      bankFrom,
+    } = req.body;
+    if (!req.file) {
+      return res.status(404).json({ message: "Image not found!!" });
+    }
+    if (
+      idItem == undefined ||
+      duration == undefined ||
+      // price == undefined ||
+      bookingStartDate == undefined ||
+      bookingEndDate == undefined ||
+      firstName == undefined ||
+      lastName == undefined ||
+      email == undefined ||
+      phoneNumber == undefined ||
+      accountHolder == undefined ||
+      bankFrom == undefined
+    ) {
+      return res.status(404).json({ message: "Lengkapi semua field!" });
+    }
+
+    const item = await Item.findById(idItem);
+    if (!item) {
+      res.status(404).json({ messag: "Item not found!!" });
+    }
+
+    item.sumBooking += 1;
+    await item.save();
+
+    let total = item.price * duration;
+    let tax = total * 0.1;
+
+    const invoice = Math.floor(1000000 + Math.random() * 9000000);
+
+    const member = await Member.create({
+      firstName,
+      lastName,
+      email,
+      phoneNumber,
+    });
+
+    const newBooking = {
+      invoice,
+      bookingStartDate,
+      bookingEndDate,
+      total: (total += tax),
+      itemId: {
+        _id: item.id,
+        title: item.title,
+        price: item.price,
+        duration: duration,
+      },
+      memberId: member.id,
+      payments: {
+        proofPayment: `images/${req.file.filename}`,
+        bankFrom: bankFrom,
+        accountHolder: accountHolder,
+      },
+    };
+
+    const booking = await Booking.create(newBooking);
+    res.status(200).json({ message: "Success Booking", booking });
   },
 };
