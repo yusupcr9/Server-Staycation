a95aba89ae7df3dfe813c610c634772ba89c2271
Yusup Firmansyah <yhusup6@gmail.com>
2021-11-21 20:59:00 +0700

    Update Footer + Access Control Allow Origin

--- app.js
+++ app.js
@@ -47,6 +47,21 @@ app.use(
   "/ckeditor",
   express.static(path.join(__dirname, "node_modules/ckeditor4"))
 );
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
+  res.header("Access-Control-Expose-Headers", "Content-Length");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Accept, Authorization, Content-Type, X-Requested-With, Range"
+  );
+  if (req.method === "OPTIONS") {
+    return res.send(200);
+  } else {
+    return next();
+  }
+});
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
